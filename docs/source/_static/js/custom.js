de9015c42c3277a5c8fe855cd6847b4f9f2eb1d9
snow <xueziweisz@126.com>
2025-01-13 13:56:35 +0800

    updae doc styles

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,9 +1,20 @@
 document.querySelectorAll('.sig-paren').forEach(el => {
     if (el.textContent.trim() === ')') {
-        if ((hasEmInHierarchy = $(el).parent().find("em").length > 0) & ($(el).parent().text().includes(', '))) {
+        if (Array.from(
+                $(el).parent().find("em")
+        ).filter(e => !e.className.includes("property")).length > 1) {
             el.style.setProperty('--line-break-content', 'block');
         }
     }
 });
+document.querySelectorAll('.sig-param').forEach(el => {
+    if (Array.from($(el).parent().find("em")).filter(e => !e.className.includes("property")).length < 2) {
+        $(el).addClass("remove-before")
+    } else {
+        var t = $($(el).find(".n")[0]).text();
+        $($(el).find(".n")[0]).text("    " + t)
+    }
+})
+
 
 $(".theme-icon-when-auto-dark").remove();
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,7 @@ extensions = [
 ]
 
 bibtex_bibfiles = ["references.bib"]
+bibtex_reference_style = "author_year"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
